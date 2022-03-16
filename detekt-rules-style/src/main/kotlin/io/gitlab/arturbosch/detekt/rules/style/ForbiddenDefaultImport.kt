3fc7eab010cae75c65ea52e0d786a7899e592ba3
Dennis Pietruck <dennis.pietruck@otto.de>
2022-03-16 12:20:47 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenDefaultImport.kt
+++ detekt-rules-style/src/main/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenDefaultImport.kt
@@ -1,12 +1,51 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
+import io.gitlab.arturbosch.detekt.api.CodeSmell
 import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
 import io.gitlab.arturbosch.detekt.api.internal.RequiresTypeResolution
+import io.gitlab.arturbosch.detekt.api.simplePatternToRegex
+import org.jetbrains.kotlin.psi.KtImportDirective
 
 @RequiresTypeResolution
 class ForbiddenDefaultImport(config: Config = Config.empty) : Rule(config) {
-    override val issue: Issue
-        get() = TODO("Not yet implemented")
+    override val issue = Issue(
+        javaClass.simpleName,
+        Severity.Style,
+        "TODO",
+        Debt.TEN_MINS
+    )
+
+    override fun visitImportDirective(importDirective: KtImportDirective) {
+        super.visitImportDirective(importDirective)
+
+        val import = importDirective.importedFqName?.asString().orEmpty()
+        if (DEFAULT_IMPORTS.any { it.matches(import) }) {
+            report(
+                CodeSmell(
+                    issue,
+                    Entity.from(importDirective),
+                    "The import " +
+                        "$import has been forbidden in the Detekt config."
+                )
+            )
+        }
+    }
+
+    private companion object {
+        private val DEFAULT_IMPORTS = listOf(
+            "kotlin.*",
+            "kotlin.annotation.*",
+            "kotlin.collections.*",
+            "kotlin.comparisons.*",
+            "kotlin.io.*",
+            "kotlin.ranges.*",
+            "kotlin.sequences.*",
+            "kotlin.text.*",
+        ).map(String::simplePatternToRegex)
+    }
 }
--- detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenDefaultImportTest.kt
+++ detekt-rules-style/src/test/kotlin/io/gitlab/arturbosch/detekt/rules/style/ForbiddenDefaultImportTest.kt
@@ -1,25 +1,56 @@
 package io.gitlab.arturbosch.detekt.rules.style
 
+import io.github.detekt.test.utils.resourceAsPath
 import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
+import io.gitlab.arturbosch.detekt.rules.setupKotlinEnvironment
 import io.gitlab.arturbosch.detekt.test.assertThat
+import io.gitlab.arturbosch.detekt.test.compileAndLint
 import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 @KotlinCoreEnvironmentTest
-internal class ForbiddenDefaultImportTest(val env: KotlinCoreEnvironment) {
-    val subject = ForbiddenDefaultImport()
+internal class ForbiddenDefaultImportTest : Spek({
+    setupKotlinEnvironment(additionalJavaSourceRootPath = resourceAsPath("java"))
 
-    @Test
-    fun `import defaults should throw error`() {
-        val code = """
+    val env: KotlinCoreEnvironment by memoized()
+    val subject by memoized { ForbiddenDefaultImport() }
+
+    describe("ForbiddenDefaultImport rule") {
+        context("report default import") {
+            it("is default import") {
+                val code = """
             import kotlin.io.DEFAULT_BUFFER_SIZE
             import kotlin.io.FileTreeWalk
             import kotlin.io.println
+            import kotlin.ranges.*
             import kotlin.Exception as KotlinException
-        """
-        val findings = subject.compileAndLintWithContext(env, code)
-        assertThat(findings).hasSize(4)
+            """
+                val findings = subject.compileAndLintWithContext(env, code)
+                assertThat(findings).hasSize(5)
+            }
+        }
+        context("ignore non defaults") {
+            it("is non default import") {
+                val code = """
+                    import kotlin.math.PI
+                    import kotlin.contracts.Returns
+                """
+                val findings = subject.compileAndLintWithContext(env, code)
+                assertThat(findings).isEmpty()
+            }
+
+            it("should not report without type resolution") {
+                val code = """
+            import kotlin.io.println
+            """
+                val findings = subject.compileAndLint(code)
+                assertThat(findings).isEmpty()
+            }
+        }
     }
 }
+)
